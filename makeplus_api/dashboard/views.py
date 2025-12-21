973dedb5c599a9982efffeba6b799bab95e3b383
DjalilElz <abdeldjalil.elazizi@ensia.edu.com>
2025-12-21 17:57:53 +0100

    fixing session creation problem6

--- makeplus_api/dashboard/views.py
+++ makeplus_api/dashboard/views.py
@@ -420,12 +420,14 @@ def event_create_step3(request):
         
         if action == 'add_session':
             form = SessionForm(request.POST, event=event)
-            # Make room field not required and set value
+            # Make room and status fields not required since we set them manually
             form.fields['room'].required = False
+            form.fields['status'].required = False
             if form.is_valid():
                 session = form.save(commit=False)
                 session.event = event
                 session.room = current_room  # Force the current room
+                session.status = 'pas_encore'  # Set default status
                 session.created_by = request.user
                 session.save()
                 messages.success(request, f'Session "{session.title}" added to {current_room.name}!')
@@ -454,9 +456,15 @@ def event_create_step3(request):
             return redirect('dashboard:event_create_step3')
     
     form = SessionForm(initial={'start_time': event.start_date, 'end_time': event.start_date}, event=event)
-    # Hide room field since we're setting it automatically
+    # Hide/disable fields that are not in the template or set automatically
     form.fields['room'].widget = form.fields['room'].hidden_widget()
     form.fields['room'].required = False
+    form.fields['status'].widget = form.fields['status'].hidden_widget()
+    form.fields['status'].required = False
+    # Make optional fields truly optional
+    form.fields['speaker_bio'].required = False
+    form.fields['speaker_photo_url'].required = False
+    form.fields['cover_image_url'].required = False
     
     context = {
         'form': form,
